c0172bd7df20d67e4c7814f6f6ed2898b132aded
alfan06 <alfannoufal@gmail.com>
2020-04-28 12:17:08 +0700

    jopsit

---
+++ laravel-crud/resources/views/simpan.blade.php
@@ -0,0 +1,34 @@
+@extends('master')
+
+<!-- Isi title -->
+@section('title','Validasi Data')
+
+<!-- Isi bagian judul halaman  -->
+@section('judul_halaman','Validasi Data Mahasiswa')
+
+<!-- Isi konten -->
+@section('konten')
+<h3 class="my-4">Data yang di Input : </h3>
+
+    <table class="table table-bordered table-striped">
+        <tr>
+            <td style="width:150px">Nama</td>
+            <td>{{$data->namamhs}}</td>
+        </tr>
+        <tr>
+            <td>NIM</td>
+            <td>{{$data->nimmhs}}</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>{{$data->emailmhs}}</td>
+        </tr>
+        <tr>
+            <td>jurusan</td>
+            <td>{{$data->jurusanmhs}}</td>
+        </tr>
+    </table>
+
+    <a href="/mahasiswa" class="btn btn-primary">Kembali</a>
+
+@endsection
---
+++ laravel-crud/resources/views/tambah.blade.php
@@ -0,0 +1,43 @@
+@extends('master')
+<!-- isi title -->
+@section('title','Tambah Data')
+
+<!-- isi bagian judul halaman -->
+@section('judul_halaman','Tambah Data Mahasiswa')
+
+<!-- isi bagian konten halaman -->
+@section('konten')
+    <a href="/mahasiswa" class="btn btn-danger">Kembali</a>
+    <br/>
+    <br/>
+    <!-- menampilkan eror validasi  -->
+    @if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <form action="/mahasiswa/simpan" method="post">
+    {{ csrf_field() }}
+        <div class="form-group">
+            <label for="namamhs">Nama</label>
+            <input type="text" class="form-control"  name="namamhs" value="{{old('namamhs')}}"><br/>
+        </div>
+        <div class="form-group">
+            <label for="nimmhs">NIM</label>
+            <input type="number" class="form-control"  name="nimmhs" value="{{old('nimmhs')}}"><br/>
+        </div>
+        <div class="form-group">
+            <label for="emailmhs">E-mail</label>
+            <input type="email" class="form-control"  name="emailmhs" value="{{old('emailmhs')}}"><br/>
+        </div>
+        <div class="form-group">
+            <label for="jurusanmhs">Jurusan</label>
+            <input type="text" class="form-control" name="jurusanmhs" value="{{old('jurusanmhs')}}"><br/>
+        </div>
+        <button type="submit" name="tambah" class="btn btn-primary float-right">Tambah Data</button>
+    </form>
+@endsection
